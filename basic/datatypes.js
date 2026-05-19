dd30476f1feee147d737ddd47649496c175acd0b
divypatel-18 <divy.dadhania122409@marwadiuniversity.ac.in>
2026-05-19 10:35:13 +0000

    datatyoes

---
+++ basic/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as new version
+
+// alert(3 + 3) // we are using node js not browser
+
+console.log(3 + 
+    3); // code redability should be high
+
+console.log("Divy");
+
+
+let name = "Divy"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// number (range) => 2 to the power 53
+// bigint
+// string => ""
+// boolean => false/true
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+// object 
+
+console.log(typeof "Divy");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
