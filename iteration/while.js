38136d2bca43ca607e906aed5f06b36784560dd9
divypatel-18 <divy.dadhania122409@marwadiuniversity.ac.in>
2026-05-21 19:29:56 +0000

    While

---
+++ iteration/while.js
@@ -0,0 +1,21 @@
+let i = 0
+
+while (i <= 10) {
+    // console.log(`value of index is ${i}`);
+    i = i + 2    
+}
+
+let Array = ["Man", "flash", "Batman"]
+
+let Arr = 0
+while (Arr < Array.length) {
+    // console.log(`Value is ${Array[Arr]}`);
+    Arr = Arr + 1
+}
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
