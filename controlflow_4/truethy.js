b81593a49159a3d8c4c52db906c35d5ca8872b55
divypatel-18 <divy.dadhania122409@marwadiuniversity.ac.in>
2026-05-21 13:51:54 +0000

    truthy

---
+++ controlflow_4/truethy.js
@@ -0,0 +1,67 @@
+// const userEmail = []
+
+// if(userEmail){
+//     console.log("got user email");
+// }else{
+//     console.log("not email");
+// }
+
+// fasly value
+// false
+// 0
+// -0
+// BigInt 0n
+// ""
+// null
+// undefined
+// NaN
+
+//truthy value
+// "0"
+// 'false' or "false"
+// " "
+// []
+// {}
+// function(){}
+
+// if(userEmail.length === 0){
+//     console.log("Array");
+    
+// }
+
+// const obj = {}
+
+// if(Object.keys(obj).length === 0){
+//     console.log("Empty");
+    
+// }
+
+// knowledge
+// false == 0
+// true 
+
+// false = ''
+// true
+
+// 0 = ''
+// true
+
+// Nullish Coalesing Operator (??): work on Null and Undefined
+
+let Val1;
+//  val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+// val1 = 5 ?? 10 ?? null
+
+//  console.log(val1);
+ 
+
+// ternary operator
+
+// condition ? true : false
+
+const tea = 100
+tea <=80 ? console.log("less than 80") : console.log("more than 80");
+
+ 
