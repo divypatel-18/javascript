30d9e9ca0c0e3e83f2482e40f961fa22566f44f6
divypatel-18 <divy.dadhania122409@marwadiuniversity.ac.in>
2026-05-22 07:01:22 +0000

    loop

---
+++ iteration/loop.js
@@ -0,0 +1,65 @@
+// for of loop
+
+const arr = [1, 2, 3, 4, 5]
+ for (const num of arr) {
+    // console.log(num);   
+ }
+
+ const greet = "Hello!"
+ for (const i of greet) {
+    //  console.log(`Each character is ${i}`);  
+ }
+
+//  map
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United State of America")
+// map.set('Fr', "France")
+// map.set('IN', "India") // Copy is not allowed
+
+// console.log(map);
+
+// for (const [key, value] of map){
+    // console.log(key, ':-', value);
+// }
+
+// const obj = {           // obj is not iteratable
+//     'game1': 'NFS',
+//     'game2': 'spiderman'
+// }
+
+// for (const [key, value] of obj) { // obj is not iteratable
+//      console.log(key, ':-', value);
+// }
+
+// const object = {
+//     js: 'javascript',
+//     cp: 'c++',
+//     rb: 'ruby',
+//     html: 'hyper text markup language'
+// }
+
+// for (const key in object) {
+//     console.log(`${key} file name is ${object[key]}`);
+    
+// }
+
+// const program = ["js", "html", "cpp", "ruby"]
+
+// for (const key in program) {
+//     console.log(program[key]);
+    
+// }
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United State of America")       // Map is not iteratable
+// map.set('Fr', "France")
+// map.set('IN', "India")                      
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
+
