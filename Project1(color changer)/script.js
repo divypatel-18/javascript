93d734636d6e901bdaded263a9e25a711fc8bed9
divypatel-18 <divy.dadhania122409@marwadiuniversity.ac.in>
2026-05-23 13:45:14 +0000

    continue

--- Project1(color changer)/script.js
+++ Project1(color changer)/script.js
@@ -1,18 +1,30 @@
-const buttons = document.querySelectorAll('.button');
-const body = document.querySelector('body');
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
 
-buttons.forEach(function (button) {
+buttons.forEach(function(button){
     button.addEventListener('click', function(e){
         if(e.target.id === 'grey'){
             body.style.backgroundColor = e.target.id;
         };
-        if(e.target.id === 'yellow'){
+        if(e.target.id === 'blue'){
             body.style.backgroundColor = e.target.id;
         };
         if(e.target.id === 'white'){
             body.style.backgroundColor = e.target.id;
         };
-        if(e.target.id === 'blue'){
+        if(e.target.id === 'green'){
+            body.style.backgroundColor = e.target.id;
+        };
+        if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        };
+        if(e.target.id === 'orange'){
+            body.style.backgroundColor = e.target.id;
+        };
+        if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id;
+        };
+        if(e.target.id === 'purple'){
             body.style.backgroundColor = e.target.id;
         };
     });
